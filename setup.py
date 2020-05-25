a03299e9fadf8e7c25cfb99d89120726879e2791
Rakesh Bute <rbute@users.noreply.github.com>
2020-05-25 22:04:19 +0530

    Enrich setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
+import os
+
 from setuptools import find_packages, setup
 
+description: str = ''
+
+with open('README.MD', 'r') as r:
+    description = r.read()
+    del r
+
 setup(
-    name='click-ui-unofficial',
+    name='clickui-unofficial',
     author='Rakesh Bute',
+    description="Unofficial UI wrapper on click commands",
+    long_description_content_type='text/markdown',
+    long_description=description,
+    author_email=os.getenv('MY_EMAIL', ''),
     version='0.2.0',
     license="BSD-3-Clause",
     packages=find_packages(),
     python_requires=">=3.*",
+    project_urls={
+        "Code": "https://github.com/rbute/clickui-unofficial",
+    },
     install_requires=[
         'click'
     ],
