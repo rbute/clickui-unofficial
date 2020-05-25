af6c5d6ad69d9b95ca8625084e2b148911c4c233
Rakesh Bute <rbute@users.noreply.github.com>
2020-05-25 20:34:24 +0530

    Add License

--- setup.py
+++ setup.py
@@ -4,10 +4,17 @@ setup(
     name='click-ui-unofficial',
     author='Rakesh Bute',
     version='0.2.0',
-    license="Not Decided Yet. Likely to use python click framework license",
+    license="BSD-3-Clause",
     packages=find_packages(),
     python_requires=">=3.*",
     install_requires=[
         'click'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
     ]
 )
